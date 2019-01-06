384dacf7c78a597d4d760c1280ecce503c7c9970
Scott H <daborg69@gmail.com>
2019-01-06 16:09:04 -0500

    Merged master into develop

--- TimeGuid/TimeGuid.cs
+++ TimeGuid/TimeGuid.cs
@@ -19,7 +19,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Slug
+namespace SlugEnt
 {
 	/// <summary>
 	/// Converts a time - 15:30:56 into a TimeGuid object represented as 3 recognizable and easily displayed characters - for instance - Ld7
@@ -30,29 +30,60 @@ namespace Slug
 		private static short rZ = 90;
 		private static short ra = 97;
 		private static short rz = 122;
-		private static short r0 = 48;
+		private static short r2 = 50;
 		private static short r9 = 57;
 
 
 
+		/// <summary>
+		/// Converts Hours into a single character.  We use the upper case letters to represent hours.
+		/// </summary>
+		/// <param name="hours"></param>
+		/// <returns></returns>
 		private static char ConvertHoursToChar(int hours) {
 			int start = rA + hours;
 			return Convert.ToChar(start);
 		}
 
+
+		/// <summary>
+		/// Converts Minutes into a single character.  Because there are 60 minutes, we have to use Uppercase, Lowercase and numbers
+		/// to represent the minutes.
+		/// </summary>
+		/// <param name="minutes"></param>
+		/// <returns></returns>
 		private static char ConvertMinutesToChar(int minutes) {
 			return ConvertTimeUnit60ToChar(minutes);
 		}
 
+
+
+		/// <summary>
+		/// Converts Seconds into a single character.  Because there are 60 seconds, we have to use Uppercase, Lowercase and numbers
+		/// to represent the seconds.
+		/// </summary>
+		/// <param name="seconds"></param>
+		/// <returns></returns>
 		private static char ConvertSecondsToChar(int seconds) {
 			return ConvertTimeUnit60ToChar(seconds);
 		}
 
-		private static char ConvertTimeUnit60ToChar(int value) {
-			if (value < 26) { return Convert.ToChar(rA + value); }
-			else if (value < 52) { return Convert.ToChar(ra + (value - 26)); }
-			else { return Convert.ToChar(r0 + (value - 52)); }
 
+
+		/// <summary>
+		/// Converts a 60 number value into a single character.  In order to preserve proper sorting (at least via ascii) we start with
+		/// numbers and then use Uppercase letters and then finally lowercase numbers.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		private static char ConvertTimeUnit60ToChar(int value) {
+			if (value < 8) { return Convert.ToChar(r2 + value); }
+			else if (value < 34) { return Convert.ToChar(rA - 8 + value); }
+			else { return Convert.ToChar(ra - 34 + value); }
+			/*			if (value < 26) { return Convert.ToChar(rA + value); }
+						else if (value < 52) { return Convert.ToChar(ra + (value - 26)); }
+						else { return Convert.ToChar(r0 + (value - 52)); }
+			*/
 		}
 
 
---
+++ TimeGuid_Test/TestTimeGUID.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Copyright 2018 Scott Herrmann
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+using System;
+using NUnit.Framework;
+using SlugEnt;
+
+
+namespace SlugEnt.UnitTest
+{
+	[TestFixture]
+	[Parallelizable]
+	public class TestTimeGuid
+	{
+		// Validate that Hour Conversion works.
+		[Test]
+		public void HourConversion_Works([Range(0, 23)] int hr) {
+			string val = CalculateTimeGuidPositionalValue(hr, "h");
+
+			DateTime d = new DateTime(2000, 01, 01, hr, 0, 0);
+
+			string guidValue = TimeGuid.ConvertTimeToChar(d);
+			string guidChar = guidValue.Substring(0, 1);
+
+			Assert.AreEqual(val, guidChar, "A1:  The hour did not convert to the expected ascii character.  Exp: " + val + "  Actual: " + guidValue);
+		}
+
+
+		// Validate that minute conversion works.
+		[Test]
+		public void MinuteConversion_Works([Range(0, 59)] int minute) {
+			string val = CalculateTimeGuidPositionalValue(minute, "m");
+
+			DateTime d = new DateTime(2000, 01, 01, 0, minute, 0);
+			string guidValue = TimeGuid.ConvertTimeToChar(d);
+			string guidChar = guidValue.Substring(1, 1);
+
+			Assert.AreEqual(val, guidChar, "A1:  The minute did not convert to the expected ascii character.  Exp: " + val + "  Actual: " + guidValue);
+		}
+
+
+
+		// Validate that Second conversion works.
+		[Test]
+		public void SecondConversion_Works([Range(0, 59)] int second) {
+			string val = CalculateTimeGuidPositionalValue(second, "s");
+
+			DateTime d = new DateTime(2000, 01, 01, 0, 0, second);
+			string guidValue = TimeGuid.ConvertTimeToChar(d);
+			string guidChar = guidValue.Substring(2, 1);
+
+			Assert.AreEqual(val, guidChar, "A1:  The second did not convert to the expected ascii character.  Exp: " + val + "  Actual: " + guidValue);
+		}
+
+
+
+		// Validate across a random sample of Hours, minutes and seconds that proper TimeGuid's are generated.
+		[Test]
+		public void TimeGUIDConversion_Works([Random(0, 23, 5)] int hr, [Random(0, 59, 5)] int minute,
+			[Random(0, 59, 5)] int second) {
+			string valSec = CalculateTimeGuidPositionalValue(second, "s");
+			string valMin = CalculateTimeGuidPositionalValue(minute, "m");
+			string valHr = CalculateTimeGuidPositionalValue(hr, "h");
+			string valCombo = valHr + valMin + valSec;
+
+
+			DateTime d = new DateTime(2000, 01, 01, hr, minute, second);
+			string guidValue = TimeGuid.ConvertTimeToChar(d);
+
+
+			Assert.AreEqual(valCombo, guidValue, "A1:  The TimeGuid was not converted correctly. Exp: " + valCombo + "  Actual: " + guidValue);
+			TestContext.WriteLine("Time TimeGuid Value produced was:  " + guidValue);
+		}
+
+
+
+		// Internal routine that performs same calculation as the TimeGuid engine should be running.
+		private string CalculateTimeGuidPositionalValue(int digit, string type) {
+			int startingValue;
+
+			if ( type == "h" ) { startingValue = 65 + digit; }
+			else {
+				if ( digit < 8 ) { startingValue = 50 + digit; }
+				else if ( digit < 34 ) { startingValue = 57 + digit; }
+				else { startingValue = 97 + digit - 34;}
+
+			}
+
+			string val = Convert.ToChar(startingValue).ToString();
+			return val;
+		}
+
+	}
+}
